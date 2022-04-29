ec971212713ef051daf127975036156527b89571
Dawn Olivier <dmarieolivier@gmail.com>
2022-04-28 23:47:06 -0400

    commit tree ad65600999f6dc3af738b4a0ef9c70a4a5658cae

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork (workActivity = "go to the office") {
+    return `This Monday, I will ${workActivity}.`;
+}
+
+function wrapAdjective(highlight='*') {
+    return function (name='special') {
+        return `You are ${highlight}${name}${highlight}!`
+    }
+}
